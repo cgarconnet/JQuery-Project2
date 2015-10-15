cb1924cfe76432133538941c8465ecbac92003b6
Christophe Garconnet <iMac@iMac-de-Christophe.local>
2015-10-15 21:55:09 +0200

    3 same piece of code but FAQs is now working

--- index.js
+++ index.js
@@ -5,7 +5,34 @@ $( "#q1" ).click(function() {
 //  alert( "Handler for .click() called." );
 //	console.log($("#q1"));
 
-//	$( "#a1" ).slideToggle( "slow");
-	$( "#a1" ).fadeToggle( "slow");
+	$( "#a1" ).slideToggle( "slow");
+//	$( "#a1" ).fadeToggle( "slow");
+
+	$("#arrow1-down,#arrow1-up").toggleClass("collapse");
+//	$("#arrow1-up").toggleClass("collapse");
+
+});
+
+$( "#q2" ).click(function() {
+//  alert( "Handler for .click() called." );
+//	console.log($("#q1"));
+
+	$( "#a2" ).slideToggle( "slow");
+//	$( "#a1" ).fadeToggle( "slow");
+
+	$("#arrow2-down,#arrow2-up").toggleClass("collapse");
+//	$("#arrow1-up").toggleClass("collapse");
+
+});
+
+$( "#q3" ).click(function() {
+//  alert( "Handler for .click() called." );
+//	console.log($("#q1"));
+
+	$( "#a3" ).slideToggle( "slow");
+//	$( "#a1" ).fadeToggle( "slow");
+
+	$("#arrow3-down,#arrow3-up").toggleClass("collapse");
+//	$("#arrow1-up").toggleClass("collapse");
 
 });
