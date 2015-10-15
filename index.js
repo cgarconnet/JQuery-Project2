5a292d9605b6debb085e2def8e33693c01951a61
Christophe Garconnet <iMac@iMac-de-Christophe.local>
2015-10-15 21:50:25 +0200

    fadeToggle is done, but arrow is still the same

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-$( ".question" ).click(function() {
+//  $( ".question" ) that's on the Class name
+// $( "#q1" ) that's on the Id name
+
+$( "#q1" ).click(function() {
 //  alert( "Handler for .click() called." );
-  $( ".answer" ).slideDown( "slow", function() {
-    // Animation complete.
-  });
+//	console.log($("#q1"));
+
+//	$( "#a1" ).slideToggle( "slow");
+	$( "#a1" ).fadeToggle( "slow");
+
 });
